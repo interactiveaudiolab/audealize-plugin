4b265127c1b949ec32f19bbc50b87bfe11c85d77
Michael Donovan <mdonovan159@gmail.com>
2016-08-15 15:09:56 -0500

    AboutComponent now references AudealizeImages

--- JUCE Modules/audealize_module/ui_components/AboutComponent.h
+++ JUCE Modules/audealize_module/ui_components/AboutComponent.h
@@ -1,39 +1,8 @@
-/*
-  ==============================================================================
-
-  This is an automatically generated GUI class created by the Projucer!
-
-  Be careful when adding custom code to these files, as only the code within
-  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
-  and re-saved.
-
-  Created with Projucer version: 4.2.3
-
-  ------------------------------------------------------------------------------
-
-  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
-  Copyright (c) 2015 - ROLI Ltd.
-
-  ==============================================================================
-*/
-
 #ifndef __JUCE_HEADER_6F5BC92BB0597534__
 #define __JUCE_HEADER_6F5BC92BB0597534__
 
-//[Headers]     -- You can add your own extra header files here --
 #include "../JuceLibraryCode/JuceHeader.h"
-//[/Headers]
 
-
-
-//==============================================================================
-/**
-                                                                    //[Comments]
-    An auto-generated component, created by the Projucer.
-
-    Describe your class and how it works here!
-                                                                    //[/Comments]
-*/
 class AboutComponent  : public Component,
                         public ButtonListener
 {
@@ -43,21 +12,18 @@ public:
     ~AboutComponent();
 
     //==============================================================================
-    //[UserMethods]     -- You can add your own custom methods in this section.
-    //[/UserMethods]
+  
+    enum ColourIds{
+        backgroundColourId = 0x2000700,
+        textColourId = 0x2000701
+    };
+    
 
     void paint (Graphics& g) override;
     void resized() override;
     void buttonClicked (Button* buttonThatWasClicked) override;
 
-    // Binary resources:
-    static const char* iallogo_svg;
-    static const int iallogo_svgSize;
-
-
 private:
-    //[UserVariables]   -- You can add your own custom variables in this section.
-    //[/UserVariables]
 
     //==============================================================================
     ScopedPointer<Label> NSF;
@@ -73,7 +39,5 @@ private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AboutComponent)
 };
 
-//[EndFile] You can add extra defines here...
-//[/EndFile]
 
 #endif   // __JUCE_HEADER_6F5BC92BB0597534__

b41e5dc7031cb78cb87a56212612cbd099bc0120
Michael Donovan <mdonovan159@gmail.com>
2016-09-02 14:45:40 -0500

    Moved Properties implementation to .cpp

--- JUCE Modules/audealize_module/utils/properties.cpp
+++ JUCE Modules/audealize_module/utils/properties.cpp
@@ -1,5 +1,70 @@
 #include "properties.h"
 
-const Identifier Audealize::Properties::propertyIds::darkMode = "darkmode";
-const Identifier Audealize::Properties::propertyIds::eqDataPath = "eqDataPath";
-const Identifier Audealize::Properties::propertyIds::reverbDataPath = "reverbDataPath";
+using namespace Audealize;
+
+const Identifier Properties::propertyIds::darkMode = "darkmode";
+const Identifier Properties::propertyIds::eqDataPath = "eqDataPath";
+const Identifier Properties::propertyIds::reverbDataPath = "reverbDataPath";
+
+void Properties::writePropertiesToFile (var properties)
+{
+    File prop_file = loadPropertiesFile ();
+
+    if (!prop_file.exists ())
+    {
+        prop_file.create ();
+    }
+
+    prop_file.replaceWithText (JSON::toString (properties));
+}
+
+File Properties::loadPropertiesFile ()
+{
+    PropertiesFile::Options properties;
+    properties.osxLibrarySubFolder = "Application Support";
+
+#ifdef JUCE_LINUX
+    properties.folderName = ".Audealize";
+#else
+    properties.folderName = "Audealize";
+#endif
+
+    properties.applicationName = "Audealize";
+    properties.filenameSuffix = "cfg";
+
+    File defaultFile = properties.getDefaultFile ();
+
+    // if no config file exists, create one using the default settings
+    if (!defaultFile.existsAsFile ())
+    {
+        DynamicObject* temp = new DynamicObject ();
+        temp->setProperty ("darkmode", DEFAULT_DARKMODE);
+        temp->setProperty (propertyIds::eqDataPath, DEFAULT_EQ_DATA_PATH);
+        temp->setProperty (propertyIds::reverbDataPath, DEFAULT_REVERB_DATA_PATH);
+
+        defaultFile.create ();
+
+        defaultFile.replaceWithText (JSON::toString (var (temp)));
+    }
+
+    return defaultFile;
+}
+
+var Properties::loadPropertiesVar ()
+{
+    var properties;
+
+    if (JSON::parse (loadPropertiesFile ().loadFileAsString (), properties).wasOk ())
+    {
+        return properties;
+    }
+
+    return var ();
+}
+
+var Properties::getProperty (Identifier propertyId)
+{
+    var property = loadPropertiesVar ().getDynamicObject ()->getProperty (propertyId);
+
+    return loadPropertiesVar ().getDynamicObject ()->getProperty (propertyId);
+}
--- JUCE Modules/audealize_module/utils/properties.h
+++ JUCE Modules/audealize_module/utils/properties.h
@@ -1,7 +1,7 @@
 //
 //  properties.h
 //
-//  Functions for saving/loading properties data.
+//  Functions for saving/loading properties data for Audealize plugins
 //
 
 #ifndef properties_h
@@ -12,7 +12,7 @@
 #define DEFAULT_REVERB_DATA_PATH "/Library/Application Support/Audealize/reverbdescriptors.json"
 
 #elif JUCE_WINDOWS
-#define DEFAULT_EQ_DATA_PATH "C:\\Program Files\\Audealize\\reverbdescriptors.json"
+#define DEFAULT_EQ_DATA_PATH "C:\\Program Files\\Audealize\\eqdescriptors.json"
 #define DEFAULT_REVERB_DATA_PATH "C:\\Program Files\\Audealize\\reverbdescriptors.json"
 
 #elif JUCE_LINUX
@@ -25,7 +25,7 @@
 
 namespace Audealize
 {
-/// Functions for saving/loading properties data.
+/// Functions for saving/loading config data.
 class Properties
 {
 public:
@@ -42,77 +42,28 @@ public:
      *
      *  @param properties
      */
-    static void writePropertiesToFile (var properties)
-    {
-        File prop_file = loadPropertiesFile ();
-
-        if (!prop_file.exists ())
-        {
-            prop_file.create ();
-        }
-
-        prop_file.replaceWithText (JSON::toString (properties));
-    }
-
+    static void writePropertiesToFile (var properties);
     /**
      *  Loads the user's configuration file or creates a new one if none exists
      *
      *  @return File
      */
-    static File loadPropertiesFile ()
-    {
-        PropertiesFile::Options properties;
-        properties.osxLibrarySubFolder = "Application Support";
-#ifdef JUCE_LINUX
-        properties.folderName = ".Audealize";
-#else
-        properties.folderName = "Audealize";
-#endif
-        properties.applicationName = "Audealize";
-        properties.filenameSuffix = "cfg";
-
-        File defaultFile = properties.getDefaultFile ();
-
-        // if no config file exists, create one using the default settings
-        if (!defaultFile.existsAsFile ())
-        {
-            DynamicObject* temp = new DynamicObject ();
-            temp->setProperty ("darkmode", DEFAULT_DARKMODE);
-            temp->setProperty (propertyIds::eqDataPath, DEFAULT_EQ_DATA_PATH);
-            temp->setProperty (propertyIds::reverbDataPath, DEFAULT_REVERB_DATA_PATH);
-
-            defaultFile.create ();
-
-            defaultFile.replaceWithText (JSON::toString (var (temp)));
-        }
-
-        return defaultFile;
-    }
+    static File loadPropertiesFile ();
 
     /**
      *  Returns a juce::var containing a list of properties
      */
-    static var loadPropertiesVar ()
-    {
-        var properties;
-        if (JSON::parse (loadPropertiesFile ().loadFileAsString (), properties).wasOk ())
-        {
-            return properties;
-        }
-        return var ();
-    }
+    static var loadPropertiesVar ();
 
     /**
      *  Returns a juce::var containing an individual property, referenced by its property
      *
      *  @see Properties::propertyIds
      */
-    static var getProperty (Identifier propertyId)
-    {
-        var property = loadPropertiesVar ().getDynamicObject ()->getProperty (propertyId);
+    static var getProperty (Identifier propertyId);
 
-        return loadPropertiesVar ().getDynamicObject ()->getProperty (propertyId);
-    }
+private:
+    Properties ();
 };
 }
 #endif /* properties_h */
